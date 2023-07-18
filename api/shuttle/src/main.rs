df97211ebe2cb090cb23293673a9f7fbd3913956
zhangjiantao <zhangjiantao@lixiang.com>
2023-07-18 18:34:38 +0800

    implement film endpoints

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -13,10 +13,13 @@ async fn actix_web(
         .await
         .map_err(CustomError::new)?;
 
-    let pool = actix_web::web::Data::new(pool);
+    let film_repository = api_lib::film_repository::PostgresFilmRepository::new(pool);
+    
+    let film_repository: actix_web::web::Data<Box<dyn api_lib::film_repository::FilmRepository>> =
+           actix_web::web::Data::new(Box::new(film_repository));
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(pool)
+        cfg.app_data(film_repository)
             .configure(api_lib::health::service)
             .configure(api_lib::films::service);
     };
