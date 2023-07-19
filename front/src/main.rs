dc2500b4d7e515b57b154ea288e26a8ac5a15f38
zhangjiantao <zhangjiantao@lixiang.com>
2023-07-19 15:31:28 +0800

    Layout Components

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,3 +1,39 @@
+#![allow(non_snake_case)]
+
+mod components;
+use components::{Footer, Header};
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
+
+    log::info!("Message on my console12");
+
+    dioxus_web::launch(App);
+
+
+}
+// Define a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+
+       main {
+           class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
+           Header {}
+           section {
+               class: "md:container md:mx-auto md:py-8 flex-1",
+           }
+           Footer {}
+       }
+    })
+}
+
+pub fn MyComponent(cx: Scope) -> Element {
+    cx.render(rsx!(
+        div {
+            class: "my-component",
+            "data-my-attribute": "my value",
+            "My component"
+        }
+    ))
 }
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: "all",
+  content: [
+      // Include all Rust, HTML, and CSS files in the src directory
+      "./src/**/*.{rs,html,css}",
+      // Include all HTML files in the output (dist) directory
+      "./dist/**/*.html",
+  ],
   theme: {
-    extend: {},
+      extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('tailwindcss-animated')
+  ]
 }
 
