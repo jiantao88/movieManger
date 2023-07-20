45de2d6a81820357d43d53fd2380bfa41ae10285
zhangjiantao <zhangjiantao@lixiang.com>
2023-07-20 15:21:26 +0800

    deploy

--- front/src/components/mod.rs
+++ front/src/components/mod.rs
@@ -1,5 +1,11 @@
 mod footer;
 mod header;
+mod button;
+mod film_modal;
+mod film_card;
 
+pub use film_modal::FilmModal;
 pub use footer::Footer;
 pub use header::Header;
+pub use button::Button;
+pub use film_card::FilmCard;
